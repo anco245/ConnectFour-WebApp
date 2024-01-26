659ac4874e367001639767aed0bc3201b8f19c9d
John Cifone <john.c6485@gmail.com>
2024-01-26 07:00:21 -0500

    added semicolons in javascript

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const gameboard = document.querySelector("#gameboard")
+const gameboard = document.querySelector("#gameboard");
 
 //graphic to represent an empty space on the board
-const empty = '<div class="piece"><svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 512 512\"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d=\"M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512z\"/></svg></div>'
+const empty = '<div class="piece"><svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 512 512\"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d=\"M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512z\"/></svg></div>';
 
 // Retrieve the height and width of the board from sessionStorage
 const storedHeight = sessionStorage.getItem("inputHeight");
@@ -14,26 +14,28 @@ document.getElementById("gameboard").style.width = (storedWidth * 60) + "px";
 const storedPlayer1Color = sessionStorage.getItem("inputPlayer1Color");
 const storedPlayer2Color = sessionStorage.getItem("inputPlayer2Color");
 
-const startPieces = []
+const startPieces = [];
 
-let turn = 0
+let turn = 0;
+
+let lastPos;
 
 
 function populateBoard() {
     for(let i = 0; i < storedHeight * storedWidth; i++)
     {
-        startPieces.push(empty)
+        startPieces.push(empty);
     }
 }
 
 function createBoard () {
     startPieces.forEach((startPieces, i) => {
-        const square = document.createElement('div')
+        const square = document.createElement('div');
 
-        square.classList.add('square')
+        square.classList.add('square');
         square.innerHTML = startPieces;
 
-        square.setAttribute('square-id', i)
+        square.setAttribute('square-id', i);
 
         //make a specific piece red or yellow
         // if(i == 1)
@@ -72,9 +74,8 @@ function insert (col) {
         }
     }
 
-    turn++
+    turn++;
 }
-
 
 
 populateBoard();
