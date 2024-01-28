4aac180bef1e31fd7eb5d38e8aac3ef24dbd88e0
John Cifone <john.c6485@gmail.com>
2024-01-27 23:57:24 -0500

    added column labels to gameboard

--- javascript/app.js
+++ javascript/app.js
@@ -1,4 +1,5 @@
 const gameboard = document.querySelector("#gameboard");
+const columnLabels = document.querySelector("#columnLabels")
 
 //graphic to represent an empty space on the board
 const empty = '<div class="piece"><svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 512 512\"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d=\"M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512z\"/></svg></div>';
@@ -11,15 +12,28 @@ const storedWidth = sessionStorage.getItem("inputWidth");
 document.getElementById("gameboard").style.height = (storedHeight * 60) + "px";
 document.getElementById("gameboard").style.width = (storedWidth * 60) + "px";
 
+document.getElementById("columnLabels").style.width = (storedWidth * 60) + "px";
+
 const storedPlayer1Color = sessionStorage.getItem("inputPlayer1Color");
 const storedPlayer2Color = sessionStorage.getItem("inputPlayer2Color");
 
 const startPieces = [];
+let labels = [];
+
+//populating div
+for(let i = 1; i <= storedWidth; i++)
+{
+  labels.push(i);
+}
 
 let currentColor;
 let turn = 0;
 let lastPos;
 let winner = 0;
+
+//export {winner};
+//export {storedPlayer1Color};
+//export {storedPlayer2Color};
 
 
 function populateBoard() {
@@ -29,17 +43,30 @@ function populateBoard() {
     }
 }
 
+function createLabels() {
+  labels.forEach((labels, i) => {
+      const square = document.createElement('div');
+
+      square.classList.add('label');
+      square.innerHTML = labels;
+
+      square.setAttribute('label-num', i);
+
+      columnLabels.append(square);
+  })
+}
+
 function createBoard() {
-    startPieces.forEach((startPieces, i) => {
-        const square = document.createElement('div');
+  startPieces.forEach((startPieces, i) => {
+      const square = document.createElement('div');
 
-        square.classList.add('square');
-        square.innerHTML = startPieces;
+      square.classList.add('square');
+      square.innerHTML = startPieces;
 
-        square.setAttribute('square-id', i);
+      square.setAttribute('square-id', i);
 
-        gameboard.append(square);
-    })
+      gameboard.append(square);
+  })
 }
 
 function insert(col) {
@@ -131,7 +158,8 @@ function vertical(squareId)
 }
 
 function leftDiagonal(squareId)
-{//for initial starting point for left to right
+{
+  //for initial starting point for left to right
 
   let count = 0;
   var upper = parseInt(squareId);
@@ -167,55 +195,46 @@ function leftDiagonal(squareId)
       count = 0;
     }
   }
-
 }
 
-  /*
-  private boolean rightDiagonal(int x, int y)
-  {
-    int startx, starty = 0;
-    int endx, endy = 0;
-    int count = 0;
 
-    //for initial starting point for right to left
-    if(x > y)
+function rightDiagonal(squareId)
+{
+  let count = 0;
+  var upper = parseInt(squareId);
+  var lower = parseInt(squareId);
+
+  if((turn-1)%2==0)
     {
-      startx = x-y;
-      starty = 0;
-
-      endx = altWide;
-      endy = altWide - startx;
-    } else if (x < y) {
-      startx = 0;
-      starty = y-x;
-
-      endx = altDeep - starty;
-      endy = altDeep;
+        currentColor = storedPlayer1Color;
     } else {
-      startx = 0;
-      starty = 0;
-
-      endx = altWide;
-      endy = altWide;
+        currentColor = storedPlayer2Color;
     }
 
-    while(startx <= endx)
-    {
-      if (count==4) {
-        return true;
-      } else if (board[startx][starty].equals(Main.currentPlayer)) {
-        count++;
-      } else if (!board[startx][starty].equals(Main.currentPlayer)) {
-        count = 0;
-      }
-
-      startx++;
-      starty++;
-    }
-
-    return count==4;
+  while(lower > (storedWidth-1))
+  {
+    lower-=(storedWidth+1);
   }
-*/
+
+  while(upper < (storedWidth * storedHeight - 1) - (storedWidth-1))
+  {
+    upper+=(storedWidth+1);
+  }
+
+  for(let i = parseInt(lower); i <= upper; i+=(storedWidth-1))
+  {
+    var square = document.querySelector('div[square-id="' + i + '"]');
+        
+    if(count==3)
+    {
+      return true;
+    } else if (square.firstChild.firstChild.classList.contains(currentColor)) {
+      count++;
+    } else if (!square.firstChild.firstChild.classList.contains(currentColor)) {
+      count = 0;
+    }
+  }
+}
 
 function hasWinner()
 {
@@ -241,7 +260,8 @@ function hasWinner()
     }
     */
 
-    if(horizontal(lastPos) || vertical(lastPos) || leftDiagonal(lastPos))
+    if(horizontal(lastPos) || vertical(lastPos) || leftDiagonal(lastPos) ||
+        rightDiagonal(lastPos))
     {
       if(currentColor == storedPlayer1Color)
       {
@@ -256,6 +276,6 @@ function hasWinner()
     return false;
 }
 
-
 populateBoard();
 createBoard();
+createLabels();
---
+++ javascript/winScreen.js
@@ -0,0 +1,3 @@
+import {winner} from 'app.js';
+import {storedPlayer1Color} from 'app.js';
+import {storedPlayer2Color} from 'app.js';
